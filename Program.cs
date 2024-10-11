436661b0129cbffa485f52e31d605019718190b1
Taypuppo <taypuppo@icloud.com>
2024-10-11 16:20:53 -0300

    iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Infraestrutura.Db;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+    options.UseSqlServer (builder.Configuration.GetConnectionString("sql")));
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
+
+app.MapPost("/login", (MinimalApi.Domain.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login com sucesso");
+    else
+        return Results.Unauthorized();
+});
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
